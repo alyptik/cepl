cbe179087cb766e56a99108a12e840a0dc9d7314
alyptik <alyptik@protonmail.com>
2017-06-27 14:27:12 -1000

    elfsyms.pl: remove extra spaces from output

--- elfsyms.pl
+++ elfsyms.pl
@@ -3,9 +3,10 @@
 use strict;
 use warnings;
 
-map { s/.*\bT\b//g && print if /\bT\s[^_]/; }
+print join(" ",
+	map { s|\s+| |gr; }
+	join(" ",
+	map { s|^.*\sT\s*(.*)$|$1\(| && $_ if /\bT\s[^_]/m; }
 	map { split /\n/, `nm -D $_`; }
-	keys %{{map { $_ => 1 }
-		map { s|.*|/lib/lib$&.so|r; } @ARGV}};
-
-print "\n";
+	keys %{{map { $_ => 1; }
+	map { s|^.*$|/lib/lib$&.so|r; } @ARGV}})) . "\n";
