e54a4c525fa302a53008e57105cfb344aa983167
alyptik <alyptik@protonmail.com>
2017-07-02 13:01:18 -1000

    src/parseopts.h: update usage information

--- src/parseopts.h
+++ src/parseopts.h
@@ -17,8 +17,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define CEPL_VERSION "CEPL v0.4.2"
-#define USAGE "[-chipvw] [-l<library>] [-I<include dir>] [-o<output.c>]\n\n\t-c: Use clang as the base compiler.\n\t-h: Show help/usage information.\n\t-i: Use icc as the base compiler.\n\t-p: Add symbols from dynamic libraries to readline completion.\n\t-v: Show version information.\n\t-w: Compile with ”-pedantic-errors -Wall -Wextra” flags.\n\t-l: Link against specified library (flag can be repeated).\n\t-I: Search directory for header files (flag can be repeated).\n\t-o: Name of the file to output source to.\n\nInput lines prefixed with a “;” are used to control internal state.\n\n\t;p[arse]: Toggle -p (shared library parsing) flag\n\t;q[uit]: Exit CEPL\n\t;r[eset]: Reset CEPL to its initial program state\n\t:w[arnings]: Toggle -w (warnings) flag"
+#define CEPL_VERSION "CEPL v0.5.1"
+#define USAGE "[-hpvw] [-c<compiler>] [-l<library>] [-I<include dir>] [-o<output.c>]\n\n\t-h: Show help/usage information.\n\t-p: Add symbols from dynamic libraries to readline completion.\n\t-v: Show version information.\n\t-w: Compile with ”-pedantic-errors -Wall -Wextra” flags.\n\t-c: Specify alternate compiler.\n\t-l: Link against specified library (flag can be repeated).\n\t-I: Search directory for header files (flag can be repeated).\n\t-o: Name of the file to output source to.\n\nInput lines prefixed with a “;” are used to control internal state.\n\n\t;p[arse]: Toggle -p (shared library parsing) flag\n\t;q[uit]: Exit CEPL\n\t;r[eset]: Reset CEPL to its initial program state\n\t:w[arnings]: Toggle -w (warnings) flag"
 /* perl script to pull symbols from shared libraries */
 #define ELF_SCRIPT "./elfsyms"
 /* pipe buffer size */
