a9282bc718022eebe6161a7bcb7b07aef0ae26a8
alyptik <alyptik@protonmail.com>
2017-07-03 16:54:02 -1000

    cepl.7: update usage information

--- src/parseopts.h
+++ src/parseopts.h
@@ -17,7 +17,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define CEPL_VERSION "CEPL v0.6.1"
+#define CEPL_VERSION "CEPL v0.6.2"
 #define USAGE "[-hpvw] [-c<compiler>] [-l<library>] [-I<include dir>] [-o<output.c>]\n\n\t-h,--help:\t\tShow help/usage information.\n\t-p,--parse:\t\tAdd symbols from dynamic libraries to readline completion.\n\t-v,--version:\t\tShow version information.\n\t-w,--warnings:\t\tCompile with ”-pedantic-errors -Wall -Wextra” flags.\n\t-c,--compiler:\t\tSpecify alternate compiler.\n\t-l:\t\t\tLink against specified library (flag can be repeated).\n\t-I:\t\t\tSearch directory for header files (flag can be repeated).\n\t-o:\t\t\tName of the file to output source to.\n\nInput lines prefixed with a “;” are used to control internal state.\n\n\t;p[arse]:\t\tToggle -p (shared library parsing) flag\n\t;q[uit]:\t\tExit CEPL\n\t;r[eset]:\t\tReset CEPL to its initial program state\n\t:w[arnings]:\t\tToggle -w (warnings) flag"
 /* perl script to parse symbols in shared libraries */
 #define ELF_SCRIPT "./elfsyms"
@@ -32,7 +32,7 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 char *const *parse_opts(int argc, char *argv[], char *const optstring, FILE **ofile);
 char **parse_libs(char *libs[]);
 
-/* NULL terminated string lists struct */
+/* struct definition for NULL terminated string array */
 struct str_list {
 	int cnt;
 	char **list;
