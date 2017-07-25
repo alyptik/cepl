6baab3b559ec4830689ec88a0f075373f83750bb
alyptik <alyptik@protonmail.com>
2017-07-25 09:40:59 -1000

    src/cepl.c: make struct for source buffers

--- src/cepl.c
+++ src/cepl.c
@@ -10,33 +10,27 @@
 #include "readline.h"
 #include "parseopts.h"
 
-/* source file templates */
-#define PROG_MAIN_START	("int main(int argc, char *argv[])\n{\n")
-#define PROG_START	("#define _BSD_SOURCE\n#define _DEFAULT_SOURCE\n#define _GNU_SOURCE\n#define _POSIX_C_SOURCE 200809L\n#define _SVID_SOURCE\n#define _XOPEN_SOURCE 700\n#define _Atomic\n#define _Static_assert(a, b)\n#define UNUSED __attribute__ ((unused))\n#include <assert.h>\n#include <ctype.h>\n#include <err.h>\n#include <errno.h>\n#include <fcntl.h>\n#include <limits.h>\n#include <math.h>\n#include <regex.h>\n#include <signal.h>\n#include <stdalign.h>\n#include <stdarg.h>\n#include <stdbool.h>\n#include <stddef.h>\n#include <stdint.h>\n#include <stdio.h>\n#include <stdlib.h>\n#include <stdnoreturn.h>\n#include <string.h>\n#include <strings.h>\n#include <time.h>\n#include <uchar.h>\n#include <wchar.h>\n#include <unistd.h>\n#include <sys/mman.h>\n#include <sys/types.h>\n#include <sys/syscall.h>\n#include <sys/wait.h>\nextern char **environ;\n\nint main(int argc UNUSED, char *argv[] UNUSED)\n{\n")
-#define PROG_INCLUDES	("#define _BSD_SOURCE\n#define _DEFAULT_SOURCE\n#define _GNU_SOURCE\n#define _POSIX_C_SOURCE 200809L\n#define _SVID_SOURCE\n#define _XOPEN_SOURCE 700\n#define _Atomic\n#define _Static_assert(a, b)\n#define UNUSED __attribute__ ((unused))\n#include <assert.h>\n#include <ctype.h>\n#include <err.h>\n#include <errno.h>\n#include <fcntl.h>\n#include <limits.h>\n#include <math.h>\n#include <regex.h>\n#include <signal.h>\n#include <stdalign.h>\n#include <stdarg.h>\n#include <stdbool.h>\n#include <stddef.h>\n#include <stdint.h>\n#include <stdio.h>\n#include <stdlib.h>\n#include <stdnoreturn.h>\n#include <string.h>\n#include <strings.h>\n#include <time.h>\n#include <uchar.h>\n#include <wchar.h>\n#include <unistd.h>\n#include <sys/mman.h>\n#include <sys/types.h>\n#include <sys/syscall.h>\n#include <sys/wait.h>\nextern char **environ;\n\n")
-#define PROG_MAIN_END	("\n\treturn 0;\n}\n")
-#define PROG_END	("\n\treturn 0;\n}\n")
-/* character lengths of buffer components */
-#define MAIN_START_SIZE	(strlen(PROG_MAIN_START) + 1)
-#define START_SIZE	(strlen(PROG_START) + 1)
-#define INCLUDES_SIZE	(strlen(PROG_INCLUDES) + 1)
-#define MAIN_END_SIZE	(MAIN_START_SIZE + strlen(PROG_MAIN_END) + 1)
-#define END_SIZE	(START_SIZE + strlen(PROG_END) + 1)
+/* struct definition for generated program sources */
+struct prog_src {
+	char *funcs;
+	char *body;
+	char *final;
+};
 
+/* truncated source */
+static struct prog_src user = {.funcs = NULL, .body = NULL, .final = NULL};
+/* actual source */
+static struct prog_src actual = {.funcs = NULL, .body = NULL, .final = NULL};
+/* source file templates */
+static char const prog_includes[] = "#define _BSD_SOURCE\n#define _DEFAULT_SOURCE\n#define _GNU_SOURCE\n#define _POSIX_C_SOURCE 200809L\n#define _SVID_SOURCE\n#define _XOPEN_SOURCE 700\n\n#include <assert.h>\n#include <ctype.h>\n#include <err.h>\n#include <errno.h>\n#include <error.h>\n#include <fcntl.h>\n#include <limits.h>\n#include <math.h>\n#include <regex.h>\n#include <signal.h>\n#include <stdalign.h>\n#include <stdarg.h>\n#include <stdbool.h>\n#include <stddef.h>\n#include <stdint.h>\n#include <stdio.h>\n#include <stdlib.h>\n#include <stdnoreturn.h>\n#include <string.h>\n#include <strings.h>\n#include <time.h>\n#include <uchar.h>\n#include <wchar.h>\n#include <unistd.h>\n#include <sys/mman.h>\n#include <sys/types.h>\n#include <sys/syscall.h>\n#include <sys/wait.h>\n\n#define _Atomic\n#define _Static_assert(a, b)\n#define UNUSED __attribute__ ((unused))\n\nextern char **environ;\n\n";
+static char const prog_start[] = "\n\nint main(int argc UNUSED, char *argv[] UNUSED)\n{\n";
+static char const prog_end[] = "\n\treturn 0;\n}\n";
 /* readline buffer */
-static char *line = NULL;
-/* beautified source buffer start block */
-static char *prog_main_start = NULL;
-/* actual source buffer start block */
-static char *prog_start = NULL;
-/* beautified source buffer end block */
-static char *prog_main_end = NULL;
-/* actual source buffer end block */
-static char *prog_end = NULL;
-/* function definition buffer */
-static char *func_buf = NULL;
+static char *line;
+/* output file */
+static FILE *ofile;
 /* compiler arg array */
-static char *const *cc_argv;
+static char **cc_argv;
 
 /* completion list of generated symbols */
 extern struct str_list comp_list;
@@ -45,45 +39,61 @@ extern bool warn_flag, parse_flag;
 
 static inline void free_buffers(void)
 {
-	if (prog_main_start)
-		free(prog_main_start);
-	if (prog_start)
-		free(prog_start);
-	if (prog_main_end)
-		free(prog_main_end);
-	if (prog_end)
-		free(prog_end);
-	if (func_buf)
-		free(func_buf);
-	prog_main_start = NULL;
-	prog_start = NULL;
-	prog_main_end = NULL;
-	prog_end = NULL;
+	/* write out program to file if applicable */
+	if (ofile) {
+		fwrite(actual.final, strlen(actual.final), 1, ofile);
+		fputc('\n', ofile);
+		fclose(ofile);
+	}
+	if (user.funcs)
+		free(user.funcs);
+	if (actual.funcs)
+		free(actual.funcs);
+	if (user.body)
+		free(user.body);
+	if (actual.body)
+		free(actual.body);
+	if (user.final)
+		free(user.final);
+	if (actual.final)
+		free(actual.final);
+	if (cc_argv)
+		free_argv(cc_argv);
+	user.body = NULL;
+	actual.body = NULL;
+	user.final = NULL;
+	actual.final = NULL;
+	cc_argv = NULL;
 }
 
 static inline void init_buffers(void)
 {
-	prog_main_start = malloc(MAIN_START_SIZE);
-	prog_start = malloc(START_SIZE);
-	prog_main_end = malloc(MAIN_END_SIZE);
-	prog_end = malloc(END_SIZE);
-	func_buf = malloc(START_SIZE);
+	user.funcs = malloc(1);
+	actual.funcs = malloc(strlen(prog_includes) + 1);
+	user.body = malloc(strlen(prog_start) + 1);
+	actual.body = malloc(strlen(prog_start) + 1);
+	user.final = malloc(strlen(prog_includes) + strlen(prog_start) + strlen(prog_end) + 3);
+	actual.final = malloc(strlen(prog_includes) + strlen(prog_start) + strlen(prog_end) + 3);
 	/* sanity check */
-	if (!prog_main_start || !prog_start || !prog_main_end || !prog_end || !func_buf) {
+	if (!user.funcs || !actual.funcs || !user.body || !actual.body || !user.final || !actual.final) {
 		free_buffers();
-		if (cc_argv)
-			free_argv((char **)cc_argv);
+		if (line)
+			free(line);
 		if (comp_list.list)
 			free_argv(comp_list.list);
-		err(EXIT_FAILURE, "%s", "error allocating initial pointers");
+		err(EXIT_FAILURE, "error allocating initial pointers at %d", __LINE__);
 	}
-	memset(prog_main_start, 0, MAIN_START_SIZE);
-	memset(prog_start, 0, START_SIZE);
-	memset(prog_main_end, 0, MAIN_END_SIZE);
-	memset(prog_end, 0, END_SIZE);
-	memcpy(prog_main_start, PROG_MAIN_START, MAIN_START_SIZE);
-	memcpy(prog_start, PROG_START, START_SIZE);
-	memset(func_buf, 0, START_SIZE);
+	/* zero source buffers */
+	memset(user.funcs, 0, 1);
+	memset(actual.funcs, 0, strlen(prog_includes) + 1);
+	memset(user.body, 0, strlen(prog_start) + 1);
+	memset(actual.body, 0, strlen(prog_start) + 1);
+	memset(user.final, 0, strlen(prog_includes) + strlen(prog_start) + strlen(prog_end) + 3);
+	memset(actual.final, 0, strlen(prog_includes) + strlen(prog_start) + strlen(prog_end) + 3);
+	/* no memcpy for user.funcs */
+	memcpy(actual.funcs, prog_includes, strlen(prog_includes) + 1);
+	memcpy(user.body, prog_start, strlen(prog_start) + 1);
+	memcpy(actual.body, prog_start, strlen(prog_start) + 1);
 }
 
 static inline void resize_buffer(char **buf, size_t offset)
@@ -139,22 +149,22 @@ int main(int argc, char *argv[])
 	rl_completion_suppress_append = 1;
 	rl_bind_key('\t', &rl_complete);
 
-	/* repeat readline() until EOF is read */
+	/* loop readline() until EOF is read */
 	while ((line = readline("\n>>> ")) && *line) {
 		/* re-enable completion if disabled */
 		rl_bind_key('\t', &rl_complete);
 		/* add to readline history */
 		add_history(line);
 		/* re-allocate enough memory for line + '\t' + ';' + '\n' + '\0' */
-		resize_buffer(&prog_main_start, 3);
-		resize_buffer(&prog_start, 3);
-		resize_buffer(&prog_main_end, 3);
-		resize_buffer(&prog_end, 3);
+		resize_buffer(&user.body, 3);
+		resize_buffer(&actual.body, 3);
+		resize_buffer(&user.final, 3);
+		resize_buffer(&actual.final, 3);
 
 		/* control sequence and preprocessor directive parsing */
 		switch (line[0]) {
 		case ';':
-			/* TODO: more command handling */
+			/* TODO: add additional commands */
 			switch(line[1]) {
 			/* clean up and exit program */
 			case 'q':
