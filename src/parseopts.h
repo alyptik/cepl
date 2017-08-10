fd8ae052a68fcd3b596ff7bc46d11119b93287ac
alyptik <alyptik@protonmail.com>
2017-08-10 09:02:14 -1000

    parseopts.h: swap pointer math with array indexing

--- src/parseopts.h
+++ src/parseopts.h
@@ -66,10 +66,10 @@ static inline void init_list(struct str_list *list_struct, char *initial_str)
 {
 	if ((list_struct->list = malloc((sizeof *list_struct->list) * ++list_struct->cnt)) == NULL)
 		err(EXIT_FAILURE, "%s", "error during initial list_ptr malloc()");
-	if ((*(list_struct->list + list_struct->cnt - 1) = malloc(strlen(initial_str) + 1)) == NULL)
+	if ((list_struct->list[list_struct->cnt - 1] = malloc(strlen(initial_str) + 1)) == NULL)
 		err(EXIT_FAILURE, "%s", "error during initial list_ptr[0] malloc()");
-	memset(*(list_struct->list + list_struct->cnt - 1), 0, strlen(initial_str) + 1);
-	memcpy(*(list_struct->list + list_struct->cnt - 1), initial_str, strlen(initial_str) + 1);
+	memset(list_struct->list[list_struct->cnt - 1], 0, strlen(initial_str) + 1);
+	memcpy(list_struct->list[list_struct->cnt - 1], initial_str, strlen(initial_str) + 1);
 }
 
 static inline void append_str(struct str_list *list_struct, char *str, size_t offset)
@@ -79,12 +79,12 @@ static inline void append_str(struct str_list *list_struct, char *str, size_t of
 		err(EXIT_FAILURE, "%s[%d] %s", "error during list_ptr", list_struct->cnt - 1, "malloc()");
 	list_struct->list = temp;
 	if (!str) {
-		*(list_struct->list + list_struct->cnt - 1) = NULL;
+		list_struct->list[list_struct->cnt - 1] = NULL;
 	} else {
-		if ((*(list_struct->list + list_struct->cnt - 1) = malloc(strlen(str) + offset + 1)) == NULL)
+		if ((list_struct->list[list_struct->cnt - 1] = malloc(strlen(str) + offset + 1)) == NULL)
 			err(EXIT_FAILURE, "%s[%d]", "error appending string to list_ptr", list_struct->cnt - 1);
-		memset(*(list_struct->list + list_struct->cnt - 1), 0, strlen(str) + offset + 1);
-		memcpy(*(list_struct->list + list_struct->cnt - 1) + offset, str, strlen(str) + 1);
+		memset(list_struct->list[list_struct->cnt - 1], 0, strlen(str) + offset + 1);
+		memcpy(list_struct->list[list_struct->cnt - 1] + offset, str, strlen(str) + 1);
 	}
 }
 
@@ -92,7 +92,7 @@ static inline void init_flag_list(struct flag_list *list_struct)
 {
 	if ((list_struct->list = malloc((sizeof *list_struct->list) * ++list_struct->cnt)) == NULL)
 		err(EXIT_FAILURE, "%s", "error during initial flag_list malloc()");
-	*(list_struct->list + list_struct->cnt - 1) = EMPTY;
+	list_struct->list[list_struct->cnt - 1] = EMPTY;
 }
 
 static inline void append_flag(struct flag_list *list_struct, enum src_flag flag)
@@ -101,7 +101,7 @@ static inline void append_flag(struct flag_list *list_struct, enum src_flag flag
 	if ((temp = realloc(list_struct->list, (sizeof *list_struct->list) * ++list_struct->cnt)) == NULL)
 		err(EXIT_FAILURE, "%s %d %s", "error during flag_list (cnt = ", list_struct->cnt, ") realloc()");
 	list_struct->list = temp;
-	*(list_struct->list + list_struct->cnt - 1) = flag;
+	list_struct->list[list_struct->cnt - 1] = flag;
 }
 
 #endif
