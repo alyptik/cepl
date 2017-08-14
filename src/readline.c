cdba030c0c99a4ad0229084a56ec6b419320be9d
alyptik <alyptik@protonmail.com>
2017-08-13 16:22:31 -1000

    readline.c: change “asm” keyword to “__asm__”

--- src/readline.c
+++ src/readline.c
@@ -13,13 +13,13 @@
 
 /* default completion list */
 char *comp_arg_list[] = {
-	"asm", "auto", "break", "case", "char", "const", "continue", "default",
+	"auto", "break", "case", "char", "const", "continue", "default",
 	"do", "double", "else", "enum", "extern", "float", "for", "goto",
 	"if", "inline", "int", "long", "register", "restrict", "return",
 	"short", "signed", "sizeof", "static", "struct", "switch",
 	"typedef", "union", "unsigned", "void", "volatile", "while",
 	"_Alignas", "_Alignof", "_Atomic", "_Bool", "_Complex", "_Generic",
-	"_Imaginary", "_Noreturn", "_Static_assert", "_Thread_local",
+	"_Imaginary", "_Noreturn", "_Static_assert", "_Thread_local", "__asm__",
 	"#pragma", "#include", "#define", "#if", "#ifdef", "#else", "#endif",
 	"bool", "true", "false", "free(", "malloc(", "realloc(", "calloc(",
 	"system(", "fork(", "pipe(", "execl(", "execv(", "kill(", "signal(",
@@ -37,6 +37,7 @@ char *generator(char const *text, int state)
 {
 	static size_t list_index, len;
 	char *name, *buf;
+	/* if no generated completions use the defaults */
 	char **completions = comp_list.list ? comp_list.list : comp_arg_list;
 	if (!state) {
 		list_index = 0;
