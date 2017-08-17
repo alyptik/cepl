48623e4a9999949af240c1b443cf1308d9324b51
alyptik <alyptik@protonmail.com>
2017-08-16 15:55:19 -1000

    vars.h: add struct and enum definitions

--- src/vars.h
+++ src/vars.h
@@ -8,6 +8,51 @@
 #ifndef VARS_H
 #define VARS_H 1
 
+#include <regex.h>
+#include <sys/types.h>
+
+/* silence linter */
+int regcomp(regex_t *preg, const char *regex, int cflags);
+int regexec(const regex_t *preg, const char *string, size_t nmatch, regmatch_t pmatch[], int eflags);
+size_t regerror(int errcode, const regex_t *preg, char *errbuf, size_t errbuf_size);
+void regfree(regex_t *preg);
+
+enum var_type {
+	T_CHAR,
+	T_STR,
+	T_INT,
+	T_DBL,
+	T_PTR,
+	T_ARR,
+	T_STRUCT,
+};
+
+/*
+ * struct var_meta {
+ *         enum var_type type;
+ *         char const *key;
+ *         void *val;
+ *         union {
+ *                 long long int_val;
+ *                 long double flt_val;
+ *                 void *ptr_val;
+ *         };
+ * };
+ */
+
+struct var_list {
+	int cnt;
+	struct {
+		enum var_type type;
+		char const *key;
+		union {
+			long long int_val;
+			long double flt_val;
+			void *ptr_val;
+		};
+	} *list;
+};
+
 int print_vars(char *const src, char *const cc_args[], char *const exec_args[]);
 
 #endif
