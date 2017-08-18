bd339e45a4ccaa31162a3056d088292131f2199e
alyptik <alyptik@protonmail.com>
2017-08-18 11:36:50 -1000

    vars.h: move append_var() to header

--- src/vars.c
+++ src/vars.c
@@ -153,43 +153,6 @@ size_t extract_id(char const *line, char **id, size_t *offset)
 	return match[1].rm_so;
 }
 
-static inline void append_var(struct var_list *list_struct, size_t size, size_t nmemb, char const *key, enum var_type type, void **val)
-{
-	void *tmp;
-	if (!list_struct || size < 1 || nmemb < 1 || !key || !val)
-		err(EXIT_FAILURE, "%s %d", "invalid arguments passed to append_var() at", __LINE__);
-	if ((tmp = realloc(list_struct->list, (sizeof *list_struct->list) * ++list_struct->cnt)) == NULL)
-		err(EXIT_FAILURE, "%s %d %s", "error during var_list (cnt = ", list_struct->cnt, ") realloc()");
-	list_struct->list = tmp;
-	list_struct->list[list_struct->cnt - 1].size = size;
-	list_struct->list[list_struct->cnt - 1].nmemb = nmemb;
-	list_struct->list[list_struct->cnt - 1].key = key;
-	list_struct->list[list_struct->cnt - 1].type = type;
-	switch (type) {
-	case T_CHR: /* fallthrough */
-	case T_INT:
-		list_struct->list[list_struct->cnt - 1].int_val = *((long long *)*val);
-		break;
-	case T_UINT:
-		list_struct->list[list_struct->cnt - 1].uint_val = *((unsigned long long *)*val);
-		break;
-	case T_DBL:
-		list_struct->list[list_struct->cnt - 1].dbl_val = *((double *)*val);
-		break;
-	case T_LDBL:
-		list_struct->list[list_struct->cnt - 1].ldbl_val = *((long double *)*val);
-		break;
-	case T_STR: /* fallthrough */
-	case T_PTR:
-		list_struct->list[list_struct->cnt - 1].ptr_val = (void *)*val;
-		break;
-	case T_OTHER: /* fallthrough */
-	/* for the default case only track the address of the object */
-	default:
-		list_struct->list[list_struct->cnt - 1].ptr_val = val;
-	}
-}
-
 int find_vars(char const *src, char *const cc_args[], char *const exec_args[])
 {
 	if (!src || !cc_args || !exec_args)
@@ -216,7 +179,7 @@ int find_vars(char const *src, char *const cc_args[], char *const exec_args[])
 
 	while (extract_id(src_tmp, &id_tmp, &off) != 0) {
 		src_tmp += off;
-		append_var(&list, 16, 1, id_tmp, extract_type(src_tmp, id_tmp), (void **)&id_tmp);
+		append_var(&list, 16, 1, id_tmp, extract_type(src_tmp, id_tmp));
 	}
 
 	if ((final = malloc(sizeof src_buffer + sizeof prog_end + (list.cnt * 16))) == NULL)
--- src/vars.h
+++ src/vars.h
@@ -42,6 +42,7 @@ struct var_list {
 		size_t nmemb;
 		char const *key;
 		enum var_type type;
+		void *data;
 		union {
 			long long int_val;
 			unsigned long long uint_val;
@@ -62,6 +63,46 @@ static inline void init_var_list(struct var_list *list_struct)
 	if ((list_struct->list = malloc((sizeof *list_struct->list) * ++list_struct->cnt)) == NULL)
 		err(EXIT_FAILURE, "%s", "error during initial var_list malloc()");
 	list_struct->list = NULL;
+}
+
+static inline void append_var(struct var_list *list_struct, size_t size, size_t nmemb, char const *key, enum var_type type)
+{
+	void *tmp;
+	/* if (!list_struct || size < 1 || nmemb < 1 || !key || !val) */
+	if (!list_struct || size < 1 || nmemb < 1 || !key)
+		err(EXIT_FAILURE, "%s %d", "invalid arguments passed to append_var() at", __LINE__);
+	if ((tmp = realloc(list_struct->list, (sizeof *list_struct->list) * ++list_struct->cnt)) == NULL)
+		err(EXIT_FAILURE, "%s %d %s", "error during var_list (cnt = ", list_struct->cnt, ") realloc()");
+	list_struct->list = tmp;
+	list_struct->list[list_struct->cnt - 1].size = size;
+	list_struct->list[list_struct->cnt - 1].nmemb = nmemb;
+	list_struct->list[list_struct->cnt - 1].key = key;
+	list_struct->list[list_struct->cnt - 1].type = type;
+	list_struct->list[list_struct->cnt - 1].data = NULL;
+
+	/* switch (type) { */
+	/* case T_CHR: [> fallthrough <] */
+	/* case T_INT: */
+	/*         list_struct->list[list_struct->cnt - 1].int_val = *((long long *)*val); */
+	/*         break; */
+	/* case T_UINT: */
+	/*         list_struct->list[list_struct->cnt - 1].uint_val = *((unsigned long long *)*val); */
+	/*         break; */
+	/* case T_DBL: */
+	/*         list_struct->list[list_struct->cnt - 1].dbl_val = *((double *)*val); */
+	/*         break; */
+	/* case T_LDBL: */
+	/*         list_struct->list[list_struct->cnt - 1].ldbl_val = *((long double *)*val); */
+	/*         break; */
+	/* case T_STR: [> fallthrough <] */
+	/* case T_PTR: */
+	/*         list_struct->list[list_struct->cnt - 1].ptr_val = (void *)*val; */
+	/*         break; */
+	/* case T_OTHER: [> fallthrough <] */
+	/* [> for the default case only track the address of the object <] */
+	/* default: */
+	/*         list_struct->list[list_struct->cnt - 1].ptr_val = val; */
+	/* } */
 }
 
 #endif
