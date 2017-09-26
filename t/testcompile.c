0ee0ea042387c942221d9b491b162419a68bbd63
alyptik <alyptik@protonmail.com>
2017-09-25 19:36:04 -1000

    testcompile.c: testcompile.c: add missing `#include`

--- t/testcompile.c
+++ t/testcompile.c
@@ -7,6 +7,7 @@
 
 #include "tap.h"
 #include "../src/compile.h"
+#include <sys/stat.h>
 
 int main(void)
 {
@@ -24,9 +25,18 @@ int main(void)
 
 	plan(3);
 
+	/* redirect stdout to /dev/null */
+	int saved_fd, null_fd;
+	if (!(null_fd = open("/dev/null", O_WRONLY, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)))
+		ERR("/dev/null open()");
+	saved_fd = dup(STDERR_FILENO);
+	dup2(null_fd, STDOUT_FILENO);
+	dup2(null_fd, STDERR_FILENO);
 	lives_ok({pipe_fd(-1, -1);}, "test living through pipe_fd() call with invalid fds.");
 	dies_ok({compile(NULL, NULL, argv);}, "die passing a NULL pointer to compile().");
 	ok(compile(src, cc_args, argv) == 0, "succeed compiling program.");
+	dup2(saved_fd, STDERR_FILENO);
+	close(null_fd);
 
 	done_testing();
 }
