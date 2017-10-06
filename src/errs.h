aa81ff54287df3ea89870be082c40700ac844068
alyptik <alyptik@protonmail.com>
2017-10-05 21:48:31 -1000

    errs.h: change error macro name to `FSTR`

--- src/errs.h
+++ src/errs.h
@@ -18,18 +18,18 @@
 
 /* errno, file, and lineno macros */
 #define ESTR		strerror(errno), __FILE__, __LINE__
-#define FLSTR		__FILE__, __LINE__
+#define FSTR		__FILE__, __LINE__
 
 /* warning macros */
 #define WARN(X)		fprintf(stderr, "%s %s:%u: \"%s\"\n", ESTR, (X))
-#define WARNX(X)	fprintf(stderr, "%s:%u \"%s\"\n", FLSTR, (X))
+#define WARNX(X)	fprintf(stderr, "%s:%u \"%s\"\n", FSTR, (X))
 #define WARNARR(X, Y)	fprintf(stderr, "%s %s:%u: \"%s[%zu]\"\n", ESTR, (X), (Y))
-#define WARNXARR(X, Y)	fprintf(stderr, "%s:%u \"%s[%zu]\"\n", FLSTR, (X), (Y))
+#define WARNXARR(X, Y)	fprintf(stderr, "%s:%u \"%s[%zu]\"\n", FSTR, (X), (Y))
 
 /* error macros */
 #define ERR(X)		do { fprintf(stderr, "%s %s:%u: \"%s\"\n", ESTR, (X)); exit(EXIT_FAILURE); } while (0)
-#define ERRX(X)		do { fprintf(stderr, "%s:%u \"%s\"\n", FLSTR, (X)); exit(EXIT_FAILURE); } while (0)
+#define ERRX(X)		do { fprintf(stderr, "%s:%u \"%s\"\n", FSTR, (X)); exit(EXIT_FAILURE); } while (0)
 #define ERRARR(X, Y)	do { fprintf(stderr, "%s %s:%u: \"%s[%zu]\"\n", ESTR, (X), (Y)); exit(EXIT_FAILURE); } while (0)
-#define ERRXARR(X, Y)	do { fprintf(stderr, "%s:%u \"%s[%zu]\"\n", FLSTR, (X), (Y)); exit(EXIT_FAILURE); } while (0)
+#define ERRXARR(X, Y)	do { fprintf(stderr, "%s:%u \"%s[%zu]\"\n", FSTR, (X), (Y)); exit(EXIT_FAILURE); } while (0)
 
 #endif
