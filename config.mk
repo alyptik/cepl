7e8fd5cc1b899dbd24c51d7c2b5b6e57577320b3
alyptik <alyptik@protonmail.com>
2017-12-20 12:48:17 -1000

    .travis.yml: change gcc6 to gcc5

--- config.mk
+++ config.mk
@@ -8,9 +8,9 @@
 DESTDIR ?=
 PREFIX ?= /usr/local
 CC ?= gcc
-OLVL ?= -Os
-CFLAGS ?= -pipe
-LDFLAGS ?= -pipe
+OLVL ?= -O2
+CFLAGS ?= -fstack-protector-strong
+LDFLAGS ?= -fstack-protector-strong
 
 # mandatory
 LD = $(CC)
@@ -26,7 +26,7 @@ SRC := $(wildcard src/*.c)
 TSRC := $(wildcard t/*.c)
 HDR := $(wildcard src/*.h) $(wildcard t/*.h)
 DEBUG := -D_DEBUG -p -pg -Og -ggdb3 -no-pie -fno-inline -Wfloat-equal -Wrestrict -Wshadow
-CPPFLAGS := -D_GNU_SOURCE -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -MMD -MP
+CPPFLAGS := -D_FORTIFY_SOURCE=2 -D_GNU_SOURCE -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -MMD -MP
 LIBS := -lelf -lhistory -lreadline
 TARGET := cepl
 MANPAGE := cepl.1
@@ -37,7 +37,9 @@ MKALL += Makefile debug.mk
 DEBUG += -fno-builtin -fno-common -fprofile-generate=./p -fsanitize=address,alignment,leak,undefined -fverbose-asm
 CFLAGS += -fPIC -fuse-ld=gold -flto -fuse-linker-plugin
 CFLAGS += -pedantic-errors -std=c11 -Wall -Wextra -Wno-missing-field-initializers -Wstrict-overflow
+CFLAGS += -fno-align-functions -fno-align-jumps -fno-align-labels -fno-align-loops -fno-strict-aliasing
 LDFLAGS += -fPIC -fuse-ld=gold -flto -fuse-linker-plugin
 LDFLAGS += -Wl,-O2,-z,relro,-z,now,--sort-common,--as-needed
+LDFLAGS += -fno-align-functions -fno-align-jumps -fno-align-labels -fno-align-loops -fno-strict-aliasing
 
 # vi:ft=make:
