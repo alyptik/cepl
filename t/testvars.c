5e4d4d8ab1b2a8609eb7eabbe9f2da8dff954e2a
alyptik <alyptik@protonmail.com>
2017-12-20 12:14:30 -1000

    .travis.yml: remove gcc builds

--- t/testvars.c
+++ t/testvars.c
@@ -16,8 +16,8 @@ int main(void)
 	STR_LIST ids = {0};
 	TYPE_LIST types = {0};
 	char const src[] =
-		/* "int a = 0, b = 0,*c = &a, *d = &b;" */
-		/* "struct { int *memb; } e = {0}, f = {0}, *g = &e, *h = &f;" */
+		"int a = 0, b = 0,*c = &a, *d = &b;"
+		"struct { int *memb; } e = {0}, f = {0}, *g = &e, *h = &f;"
 		"char wark[] = \"wark\", *ptr = wark;"
 		"long foo = 1, bar = 456;";
 	char const src_alt[] =
@@ -27,7 +27,7 @@ int main(void)
 		"int plonk[5] = {1,2,3,4,5}, vroom[5] = {0};"
 		"struct foo kabonk = {0};";
 
-	plan(16);
+	plan(24);
 
 	/* initialize lists */
 	init_list(&ids, NULL);
@@ -35,15 +35,15 @@ int main(void)
 
 	ok(extract_type("unsigned long long foo = 5", "foo") == T_UINT, "succeed extracting unsigned type from `foo`.");
 	ok(extract_type("int baz[] = {5,4,3,2,1,0}", "baz") == T_PTR, "succeed extracting pointer type from `baz`.");
-	ok(find_vars(src, &ids, &types) == 3, "succeed finding twelve objects.");
-	/* ok(extract_type(src, "a") == T_INT, "succeed extracting signed type from `a`."); */
-	/* ok(extract_type(src, "b") == T_INT, "succeed extracting signed type from `b`."); */
-	/* ok(extract_type(src, "c") == T_PTR, "succeed extracting pointer type from `c`."); */
-	/* ok(extract_type(src, "d") == T_PTR, "succeed extracting pointer type from `d`."); */
-	/* ok(extract_type(src, "e") == T_OTHER, "succeed extracting struct type from `e`."); */
-	/* ok(extract_type(src, "f") == T_OTHER, "succeed extracting struct type from `f`."); */
-	/* ok(extract_type(src, "g") == T_OTHER, "succeed extracting struct type from `g`."); */
-	/* ok(extract_type(src, "h") == T_OTHER, "succeed extracting struct type from `h`."); */
+	ok(find_vars(src, &ids, &types) == 12, "succeed finding twelve objects.");
+	ok(extract_type(src, "a") == T_INT, "succeed extracting signed type from `a`.");
+	ok(extract_type(src, "b") == T_INT, "succeed extracting signed type from `b`.");
+	ok(extract_type(src, "c") == T_PTR, "succeed extracting pointer type from `c`.");
+	ok(extract_type(src, "d") == T_PTR, "succeed extracting pointer type from `d`.");
+	ok(extract_type(src, "e") == T_OTHER, "succeed extracting struct type from `e`.");
+	ok(extract_type(src, "f") == T_OTHER, "succeed extracting struct type from `f`.");
+	ok(extract_type(src, "g") == T_OTHER, "succeed extracting struct type from `g`.");
+	ok(extract_type(src, "h") == T_OTHER, "succeed extracting struct type from `h`.");
 	ok(extract_type(src, "wark") == T_STR, "succeed extracting string type from `wark`.");
 	ok(extract_type(src, "ptr") == T_STR, "succeed extracting string type from `ptr`.");
 	ok(extract_type(src, "foo") == T_INT, "succeed extracting signed type from `foo`.");
