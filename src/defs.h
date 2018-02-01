e1339fee0c69b6e85537e050d811d42e37143789
Joey Pabalinas <joeypabalinas@gmail.com>
2018-02-01 00:27:39 -1000

    defs.h: add for each macro

--- src/defs.h
+++ src/defs.h
@@ -18,6 +18,7 @@
 /* macros */
 #define DEFAULT(ARG, ALT)	((ARG) ? (ARG) : (ALT))
 #define ARR_LEN(ARR)		((sizeof (ARR)) / (sizeof (ARR)[0]))
+#define FOR_EACH_IN(LIST)	for (size_t i = 0; i < (LIST).cnt; i++)
 
 /* `malloc()` wrapper */
 #define xmalloc(TYPE, PTR, SZ, MSG) \
