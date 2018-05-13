b9577319c7eecb66742b1b2801de07927e03450c
Joey Pabalinas <joeypabalinas@gmail.com>
2018-05-12 21:45:28 -1000

    cepl.1: update version strings

--- src/defs.h
+++ src/defs.h
@@ -55,7 +55,7 @@
 	} while (0)
 
 /* global version and usage strings */
-#define VERSION_STRING	"CEPL v5.8.7"
+#define VERSION_STRING	"CEPL v6.0.0"
 #define USAGE_STRING \
 	"[-hptvw] [-(a|i)<asm.s>] [-c<compiler>] [-e<code>] " \
 	"[-l<libs>] [-I<includes>] [-o<out.c>]\n\t" \
