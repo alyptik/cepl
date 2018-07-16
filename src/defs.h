67d218ce34228138495c5544e139d2dba0d77b9e
Joey Pabalinas <joeypabalinas@gmail.com>
2018-07-15 16:06:25 -1000

    hist.c: clean up source template

--- src/defs.h
+++ src/defs.h
@@ -55,7 +55,7 @@
 	} while (0)
 
 /* global version and usage strings */
-#define VERSION_STRING	"CEPL v6.0.5"
+#define VERSION_STRING	"CEPL v6.1.0"
 #define USAGE_STRING \
 	"[-hptvw] [-(a|i)<asm.s>] [-c<compiler>] [-e<code>] " \
 	"[-l<libs>] [-I<includes>] [-o<out.c>]\n\t" \
