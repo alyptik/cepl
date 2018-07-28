a1f1909e55d19a6b40b7b2a8dae469ca10df8339
Joey Pabalinas <joeypabalinas@gmail.com>
2018-07-28 11:42:03 -1000

    cepl.1: fix version typos
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- src/defs.h
+++ src/defs.h
@@ -55,7 +55,7 @@
 	} while (0)
 
 /* global version and usage strings */
-#define VERSION_STRING	"CEPL v6.3.0"
+#define VERSION_STRING	"CEPL v6.2.2"
 #define USAGE_STRING \
 	"[-hptvw] [-(a|i)<asm.s>] [-c<compiler>] [-e<code>] " \
 	"[-l<libs>] [-I<includes>] [-o<out.c>]\n\t" \
