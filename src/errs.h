08d2a2e7329320db104ee77826103f24a82d6083
Joey Pabalinas <joeypabalinas@gmail.com>
2018-10-23 09:50:37 -1000

    linting: improve readability of function-like macros
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- src/errs.h
+++ src/errs.h
@@ -16,19 +16,25 @@
 #include <string.h>
 #include <unistd.h>
 
-/* errno, file, and line number macros */
-#define ESTR		strerror(errno), __FILE__, __LINE__
-#define FSTR		__FILE__, __LINE__
-
 /* warning macros */
-#define WARN(FMT, ...) \
-	do { fprintf(stderr, "`%s`: [%s:%u] " FMT "\n", ESTR, __VA_ARGS__); } while (0)
-#define WARNX(FMT, ... ) \
-	do { fprintf(stderr, "[%s:%u] " FMT "\n", FSTR, __VA_ARGS__); } while (0)
+# define WARN(FMT, ...) \
+	do { \
+		fprintf(stderr, "`%s`: [%s:%u] " FMT "\n", strerror(errno), __FILE__, __LINE__, __VA_ARGS__); \
+	} while (0)
+# define WARNX(FMT, ... ) \
+	do { \
+		fprintf(stderr, "[%s:%u] " FMT "\n", __FILE__, __LINE__, __VA_ARGS__); \
+	} while (0)
 /* error macros */
-#define ERR(FMT, ...) \
-	do { fprintf(stderr, "`%s`: [%s:%u] " FMT "\n", ESTR, __VA_ARGS__); exit(EXIT_FAILURE); } while (0)
-#define ERRX(FMT, ...) \
-	do { fprintf(stderr, "[%s:%u] " FMT "\n", FSTR, __VA_ARGS__); exit(EXIT_FAILURE); } while (0)
+# define ERR(FMT, ...) \
+	do { \
+		fprintf(stderr, "`%s`: [%s:%u] " FMT "\n", strerror(errno), __FILE__, __LINE__, __VA_ARGS__); \
+		exit(EXIT_FAILURE); \
+	} while (0)
+# define ERRX(FMT, ...) \
+	do { \
+		fprintf(stderr, "[%s:%u] " FMT "\n", __FILE__, __LINE__, __VA_ARGS__); \
+		exit(EXIT_FAILURE); \
+	} while (0)
 
 #endif /* !defined(ERRS_H) */
