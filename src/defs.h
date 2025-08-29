e3195d349d22ed562efe1c04d579dfb91bd21da0
Joey Pabalinas <joeypabalinas@gmail.com>
2025-08-29 06:38:12 -1000

    _cepl: add -L to completion
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- src/defs.h
+++ src/defs.h
@@ -53,7 +53,7 @@
 	} while (0)
 
 /* global version and usage strings */
-#define VERSION_STRING	"cepl-9.1.0"
+#define VERSION_STRING	"cepl-9.1.1"
 #define USAGE_STRING \
 	"[-hptvw] [-c<compiler>] [-e<code to evaluate>] [-f<file>] " \
 	"[-l<library>] [-I<include directory>] [-L<library directory>] " \
